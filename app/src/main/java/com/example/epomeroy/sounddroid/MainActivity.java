976ddf7ffe44846ac78fd994057357f88b3b832e
Earl Pomeroy <earl.pomeroy42@gmail.com>
2015-09-28 00:38:33 -0600

    Added pause/play. Cleaned up app on destroy Added getRecentSongs Added searching UI

--- app/src/main/java/com/example/epomeroy/sounddroid/MainActivity.java
+++ app/src/main/java/com/example/epomeroy/sounddroid/MainActivity.java
@@ -21,7 +21,9 @@ import com.example.epomeroy.sounddroid.soundcloud.Track;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import retrofit.Callback;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Track> tracksList;
     private MediaPlayer mediaPlayer;
+    private ImageView playerStates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +47,27 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                mp.start();
+                setPlayState();
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playerStates.setImageResource(R.drawable.ic_play);
             }
         });
 
         Toolbar view = (Toolbar) findViewById(R.id.player_toolbar);
         final TextView selectedView = (TextView) findViewById(R.id.selected_title);
         final ImageView selectedImage = (ImageView) findViewById(R.id.selected_thumbnail);
+        playerStates = (ImageView) findViewById(R.id.player_states);
+
+        playerStates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setPlayState();
+            }
+        });
 
         RecyclerView rv = (RecyclerView) findViewById(R.id.songs_list);
         rv.setLayoutManager(new LinearLayoutManager(this));
@@ -60,12 +77,17 @@ public class MainActivity extends AppCompatActivity {
         tracksAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Track track = tracksList.get(position);
-                selectedView.setText(track.getTitle());
-                Picasso.with(MainActivity.this).load(track.getAvatarURL()).into(selectedImage);
+                Track selectedTrack = tracksList.get(position);
+                selectedView.setText(selectedTrack.getTitle());
+                Picasso.with(MainActivity.this).load(selectedTrack.getAvatarURL()).into(selectedImage);
+
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
 
                 try {
-                    mediaPlayer.setDataSource(track.getStreamUrl() + "?client_id=" + SoundCloudService.CLIENT_ID);
+                    mediaPlayer.setDataSource(selectedTrack.getStreamUrl() + "?client_id=" + SoundCloudService.CLIENT_ID);
                     mediaPlayer.prepareAsync();
                 } catch (IOException e) {
                     Log.d(TAG, e.getMessage());
@@ -76,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         rv.setAdapter(tracksAdapter);
 
         SoundCloudService service = SoundCloud.getService();
-        service.searchSongs("dark horse", new Callback<List<Track>>() {
+        service.getMostRecentSongs(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), new Callback<List<Track>>() {
             @Override
             public void success(List<Track> tracks, Response response) {
                 tracksList.clear();
@@ -99,6 +121,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -106,10 +142,20 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.search_view) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setPlayState() {
+        if (this.mediaPlayer.isPlaying()) {
+            this.playerStates.setImageResource(R.drawable.ic_play);
+            this.mediaPlayer.pause();
+        } else {
+            this.playerStates.setImageResource(R.drawable.ic_pause);
+            this.mediaPlayer.start();
+        }
     }
 }
